f27b1fcb2ad1b970eb576abcde79f7c2d43de093
scott japs <scott.japs@gmail.com>
2012-11-06 14:26:37 -0600

    updated format

--- Calculator.java
+++ Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
 	private String operator;
 	private double memory;
 	
+	//constructor
 	public Calculator(){
 		currentInt = "0";
 		previousInt = null;
@@ -12,16 +13,22 @@ public class Calculator {
 		memory = 0;
 	}
 	
+	//this function helps keep strings in the correct format when new things are added
 	private void addToCurrentString(String x){
 		if (currentInt == "0" || currentInt == null) currentInt = x;                                   	//prevents mass zeros at the start
 		else if(currentInt.equals("-0")) currentInt = "-" + x;                                         	//prevents mass zeros if there is a negitive
 		else if(x == "-" && currentInt.contains("-")) currentInt = currentInt.replace("-", "");			//prevents error with the negitive sign
-		else if(x == "-") currentInt = "-" + currentInt;												//same as above
+		else if(x == "-") currentInt = "-" + currentInt;												//adds negitive sign
 		else currentInt = currentInt + x;
 	}
 	
+	
+	// This function draws the calculator, including updating the displayed text.
+	
+	
 	public void drawCalculator(){
 		StdDraw.clear();
+		
 		//sets up the background and the display box
         StdDraw.setPenColor(StdDraw.GRAY);
         StdDraw.setXscale(0, 1000);
@@ -31,6 +38,7 @@ public class Calculator {
         StdDraw.filledRectangle(500, 900, 500, 100);
         StdDraw.setPenColor(StdDraw.BLACK);
         StdDraw.setPenRadius(.02);
+        
         //draws the lines for the calculator
         StdDraw.line(0, 0, 0, 1000);
         StdDraw.line(0, 0, 1000, 0);
@@ -46,6 +54,7 @@ public class Calculator {
         StdDraw.line(0, 450, 1000, 450);
         StdDraw.line(0, 600, 1000, 600);
         StdDraw.line(0, 700, 1000, 700);
+        
         //text in the boxes
         StdDraw.text(100, 75, "0");
         StdDraw.text(300, 75, ".");
@@ -77,59 +86,69 @@ public class Calculator {
         StdDraw.text(700, 525, "^");
         StdDraw.text(900, 650, "e");
         StdDraw.text(700, 650, "Backspace");
+        
         //draws the numbers in the display area
-        if(currentInt == null) StdDraw.textRight(900,900, previousInt);
-        else StdDraw.textRight(900,900, currentInt);
+        //the priority is to display currentInt, but if it is not set yet, to display previousInt
+        if(currentInt == null) StdDraw.textRight(900, 900, previousInt);
+        else StdDraw.textRight(900, 900, currentInt);
         if(operator != null) StdDraw.textLeft(100, 900, operator);
 	}
 	
-	private double factorial(double x, double acc){                          // factorial for use in sin and cos, tail recursive
+	// factorial for use in sin and cos, tail recursive
+	private double factorial(double x, double acc){                          
 		if (x == 1) return acc;
 		else if (x>1)  return factorial(x-1, x*acc);
 		else return 0;
 	}
 	
-	private double findSinIter(double x, double acc, double exp){            //my own sine function
+	//my own sine function, tail recursive
+	private double findSinIter(double x, double acc, double exp){            
 		double next;
 		//find the next element in the series
-		if ((exp-1)%4 == 0) {     //add next 
+		if ((exp - 1) % 4 == 0) {     //add next 
 			next = Math.pow(x, exp)/factorial(exp,1);
 		}
 		else{
 			next = -Math.pow(x, exp)/factorial(exp,1);
 		}
-		if (Math.abs(next)<.00000001){
+		//Is it close enough to the real answer?
+		if (Math.abs(next) < .00000001){
 			return acc + next;
 		}
 		else return findSinIter(x, acc+next, exp+2);
 	}
 	
-	private double findCosIter(double x, double acc, double exp){              //my own conine function
+	//my own cosine function, tail recursive
+	private double findCosIter(double x, double acc, double exp){              
 		double next;
 		//find the next element in the series
-		if ((exp)%4 == 0) {     //add next 
+		if ((exp) % 4 == 0) {     //add next 
 			next = Math.pow(x, exp)/factorial(exp,1);
 		}
 		else{
-			next = -Math.pow(x, exp)/factorial(exp,1);
+			next = -Math.pow(x, exp) / factorial(exp, 1);
 		}
-		if (Math.abs(next)<.00000001){
+		//is it close enough to the real answer?
+		if (Math.abs(next) < .00000001){
 			return acc + next;
 		}
-		else return findCosIter(x, acc+next, exp+2);
+		else return findCosIter(x, acc + next, exp + 2);
 	}
 	
+	//my square root function
 	private double findsqrt(double num, double prev){
-		if(Math.abs(num/prev-prev)<.0001) return prev;
-		else return findsqrt(num, (prev + num/prev)/2);
+		//is it close enough?
+		if(Math.abs(num / prev - prev) < .0001) return prev;
+		else return findsqrt(num, (prev + num / prev) / 2);
 	}
-
 	
+	//is there enough information to evaluate this?
 	private boolean canEvaluate(){
 		if(operator != null && currentInt != null && previousInt != null) return true;
 		else return false;
 	}
-	//evaluates the input with basic operations, sadly you can't use switch statements with strings in java 1.6
+	
+	//evaluates the input with basic operations, you can't use switch statements with strings in java 1.6
 	private String evaluate(){
 		double x = Double.parseDouble(previousInt);
 		double y = Double.parseDouble(currentInt);
@@ -137,54 +156,57 @@ public class Calculator {
 		String ourOperator = operator;
 		operator = null;
 		if(ourOperator == "+") return Double.toString(x+y);
-		if(ourOperator == "*") return Double.toString(x*y);
-		if(ourOperator == "-") return Double.toString(x-y);
-		if(ourOperator == "/") return Double.toString(x/y);
+		if(ourOperator == "*") return Double.toString(x * y);
+		if(ourOperator == "-") return Double.toString(x - y);
+		if(ourOperator == "/") return Double.toString(x / y);
 		if(ourOperator == "^") return Double.toString(Math.pow(x, y));
 		return "error";
 	}
-	//gets the input and adds it to the holding strings
+	
+	
+	
+	//gets the input and adds it to the holding strings depending on where the user clicked
 	public void inputs(double x, double y){
-		if (x > 0 && x < 200 && y > 0 && y < 150){                       //0
+		if (x > 0 && x < 200 && y > 0 && y < 150){                              //0
 			addToCurrentString("0");
 		}
-		else if(x > 200 && x < 400 && y > 0 && y < 150){                    // .
+		else if(x > 200 && x < 400 && y > 0 && y < 150){                        // .
 			currentInt = currentInt + ".";
 		}
-		else if(x > 400 && x < 600 && y > 0 && y < 150){                  // -
+		else if(x > 400 && x < 600 && y > 0 && y < 150){                        // -
 			addToCurrentString("-");
 		}
-		else if(x > 0 && x < 200 && y > 150 && y < 300){                  //1
+		else if(x > 0 && x < 200 && y > 150 && y < 300){                        //1
 			addToCurrentString("1");
 		}
-		else if(x > 200 && x < 400 && y > 150 && y < 300){                   //2
+		else if(x > 200 && x < 400 && y > 150 && y < 300){                      //2
 			addToCurrentString("2");
 		}
 		else if(x > 400 && x < 600 && y > 150 && y < 300){                      //3
 			addToCurrentString("3");
 		}
-		else if(x > 0 && x < 200 && y > 300 && y < 450){                   //4
+		else if(x > 0 && x < 200 && y > 300 && y < 450){                         //4
 			addToCurrentString("4");
 		}
-		else if(x > 200 && x < 400 && y > 300 && y < 450){              //5
+		else if(x > 200 && x < 400 && y > 300 && y < 450){                      //5
 			addToCurrentString("5");
 		}
-		else if(x > 400 && x < 600 && y > 300 && y < 450){                 //6
+		else if(x > 400 && x < 600 && y > 300 && y < 450){                      //6
 			addToCurrentString("6");
 		}
-		else if(x > 0 && x < 200 && y > 450 && y < 600){                  //7
+		else if(x > 0 && x < 200 && y > 450 && y < 600){                       //7
 			addToCurrentString("7");
 		}
-		else if(x > 200 && x < 400 && y > 450 && y < 600){                 //8
+		else if(x > 200 && x < 400 && y > 450 && y < 600){                      //8
 			addToCurrentString("8");
 		}
-		else if(x > 400 && x <600 && y > 450 && y < 600){                  //9
+		else if(x > 400 && x <600 && y > 450 && y < 600){                       //9
 			addToCurrentString("9");
 		}
-		else if(x > 800 && x < 1000 && y > 0 && y < 150){                 //=
+		else if(x > 800 && x < 1000 && y > 0 && y < 150){                          //=
 			if (canEvaluate()) currentInt = evaluate();
 		}
-		else if(x > 600 && x < 800 && y > 0 && y < 150){                      //*
+		else if(x > 600 && x < 800 && y > 0 && y < 150){                          //*
 			if (canEvaluate()) currentInt = evaluate();
 			if (currentInt != null){
 				previousInt = currentInt;
@@ -200,7 +222,7 @@ public class Calculator {
 			}
 			operator = "/";
 		}
-		else if(x > 800 && x < 1000 && y > 150 && y < 300){                          //-
+		else if(x > 800 && x < 1000 && y > 150 && y < 300){                         //-
 			if (canEvaluate()) currentInt = evaluate();
 			if (currentInt != null){
 				previousInt = currentInt;
@@ -208,7 +230,7 @@ public class Calculator {
 			}
 			operator = "-";
 		}
-		else if(x > 800 && x < 1000 && y > 300 && y < 450){                  //+
+		else if(x > 800 && x < 1000 && y > 300 && y < 450){                        //+
 			if (canEvaluate()) currentInt = evaluate();
 			if (currentInt != null){
 				previousInt = currentInt;
@@ -216,67 +238,63 @@ public class Calculator {
 			}
 			operator = "+";
 		}
-		else if(x>800 && x<1000 && y>700 &&y<800){                            // clear
-			operator = null;
+		else if(x > 800 && x < 1000 && y > 700 &&y < 800){                               // clear
+			operator = null; 
 			previousInt = null;
 			currentInt = "0";
 		}
-		else if(x>0 && x<200 && y>600 && y<700){                                   //sin
+		else if(x > 0 && x < 200 && y > 600 && y < 700){                                   //sin
 			if (canEvaluate()) currentInt = evaluate();
 			if (currentInt != null){
 				double forSin = Double.parseDouble(currentInt);
 				currentInt = Double.toString(findSinIter(forSin, 0, 1));
-				//currentInt = Double.toString(Math.sin(forSin));
 			}
 		}
-		else if (x>200 && x<400 && y>600 && y<700){                                  //cos
+		else if (x > 200 && x < 400 && y > 600 && y < 700){                                 //cos
 			if (canEvaluate()) currentInt = evaluate();
 			if (currentInt != null){
 				double forCos = Double.parseDouble(currentInt);
 				currentInt = Double.toString(findCosIter(forCos, 1, 2));
-				//currentInt = Double.toString(Math.cos(forCos));
 			}
 		}
-		else if (x>400 && x<600 && y >600 &&y<700){                                 //tan
+		else if (x > 400 && x < 600 && y > 600 &&y < 700){                                 //tan
 			if (canEvaluate()) currentInt = evaluate();
 			if (currentInt != null){
 				double forTan = Double.parseDouble(currentInt);
 				currentInt = Double.toString(findSinIter(forTan, 0, 1)/findCosIter(forTan, 1, 2));
-				//currentInt = Double.toString(Math.tan(forTan));
 			}
 		}
-		else if (x>800 && x< 1000 && y>450 && y<600){                               //pi
+		else if (x > 800 && x < 1000 && y > 450 && y < 600){                                 //pi
 			currentInt = "3.1415926535";
 		}
-		else if (x>600 && x<800 && y<450 && y>300){                                 //sqrt
+		else if (x > 600 && x < 800 && y < 450 && y > 300){                            //sqrt
 			if (canEvaluate()) currentInt = evaluate();
 			if (currentInt != null && Double.parseDouble(currentInt) >= 0){
 				currentInt = Double.toString(findsqrt(Double.parseDouble(currentInt), 1));
-				//currentInt = Double.toString(Math.sqrt(Double.parseDouble(currentInt)));
 			}
 		}
-		else if (x > 0 && x < 200 && y > 700 && y < 800){                          //memory add
+		else if (x > 0 && x < 200 && y > 700 && y < 800){                             //memory add
 			if (canEvaluate()) currentInt = evaluate();
 			if (currentInt != null){
 				double toUse = Double.parseDouble(currentInt);
 				memory += toUse;				
 			}
 		}
-		else if (x > 200 && x < 400 && y> 700 && y < 800){                            //memory subtract
+		else if (x > 200 && x < 400 && y > 700 && y < 800){                            //memory subtract
 			if (canEvaluate()) currentInt = evaluate();
 			if (currentInt != null){
 				double toUse = Double.parseDouble(currentInt);
 				memory -= toUse;
 			}
 		}
-		else if (x > 400 && x < 600 && y> 700 && y < 800){                            //memory clear
+		else if (x > 400 && x < 600 && y > 700 && y < 800){                            //memory clear
 			memory = 0;
 		}
-		else if (x > 600 && x < 800 && y> 700 && y < 800){                            //show memory
+		else if (x > 600 && x < 800 && y > 700 && y < 800){                            //show memory
 			currentInt = Double.toString(memory);
 			
 		}
-		else if (x>600 && x<800 && y>450 && y<600){                                       // ^
+		else if (x > 600 && x < 800 && y > 450 && y < 600){                                // ^
 			if (canEvaluate()) currentInt = evaluate();
 			if (currentInt != null){
 				previousInt = currentInt;
@@ -284,12 +302,13 @@ public class Calculator {
 			}
 			operator = "^";
 		}
-		else if (x> 800 && x<1000 && y>600 && y<700){                                 // e
+		else if (x > 800 && x < 1000 && y > 600 && y < 700){                              // e
 			currentInt = "2.71928182846";
 		}
-		else if (x>600 && x<800 && y>600 && y<700){                                    //backspace
-			if (currentInt != null && currentInt.length() >= 2) currentInt = currentInt.substring(0, currentInt.length()-1);
-			else if(currentInt.length()<=1) currentInt = "0";
+		else if (x > 600 && x < 800 && y > 600 && y < 700){                                 //backspace
+			if (currentInt != null && currentInt.length() >= 2) currentInt = currentInt.substring(0, currentInt.length() - 1);
+			else if(currentInt == null) operator = null;  
+			else if(currentInt.length() <= 1) currentInt = "0";
 		}
 		
 	}
--- README.txt
+++ README.txt
@@ -1,4 +1,7 @@
 Basic-Calculator
 ================
 
-A basic calculator I made.  I tried to limit the use of the java math library when reasonable.
+A basic calculator I made.  I tried to limit the use of the java math library when reasonable which ended up being everything other than powers.
+
+There is a dependancy for this project which is the StdDraw.java library from Algorithms 4th edition which can be found here:
+http://algs4.cs.princeton.edu/code/
